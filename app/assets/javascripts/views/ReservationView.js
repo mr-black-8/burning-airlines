449633adc868b9d94441843720754322f4c6f684
panza87 <sdpanzarino@hotmail.com>
2016-06-17 10:16:41 +1000

    Finished styling, include toast

--- app/assets/javascripts/views/ReservationView.js
+++ app/assets/javascripts/views/ReservationView.js
@@ -47,7 +47,7 @@ app.ReservationView = Backbone.View.extend({
     var view = this;
     debugger;
     if (alreadyThere) {
-      alert("You already have a seat, greedy!")
+      Materialize.toast('1 seat per person. NO SEAT FOR YOU', 4000)
     } else {
       if (seatMap && seatMap[row][col] === false) {
         // console.log(this);
@@ -61,6 +61,7 @@ app.ReservationView = Backbone.View.extend({
 
           });
         }
+        Materialize.toast('Seat booked! Refer to your reservations page for details', 4000)
       }
     }
 
